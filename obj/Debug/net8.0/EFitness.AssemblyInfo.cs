dd6cbf212348d88ab9c2a409ba7145049c659c36
Adam <96779287+ByteAdam@users.noreply.github.com>
2024-12-05 07:19:56 +0100

    poslovenjeno, lepši instruktor page

--- obj/Debug/net8.0/EFitness.AssemblyInfo.cs
+++ obj/Debug/net8.0/EFitness.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFitness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd432615938c8e12da835669fa2ad3b5f3d05e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d79a1e9a3f107697a870c8ea487d5096f4e3b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFitness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFitness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
